7ea0c932d55d29007c10e2169deaba071af3bb38
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-04-22 01:16:24 +0530

    Add @radix-ui/react-dialog dependency and update Cart component in src/components/Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,23 @@
+"use client"
+
 import React from 'react'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import { ShoppingCart } from 'lucide-react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <Sheet>
+      <SheetTrigger className="group -m-2 items-center p-2">
+        <ShoppingCart
+          aria-hidden='true'
+          className='h-6 flex-shrink-0 text-gray-400  group-hover:text-gray-500'
+        />
+        <span className='ml-2 text-sm flex-shrink font-medium  text-gray-700 group-hover:text-gray-800 '>
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className= "" ></SheetContent>
+    </Sheet>
   )
 }
 
