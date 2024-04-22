6e2d49cf3379ad9579a2831f873c8f77e1ac265e
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-04-22 22:32:08 +0530

    Update dependencies and add new packages

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,10 +2,19 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
     slug: "users", 
-    auth:true, 
+    auth: true, 
+    access: {
+      read: () => true,
+      create:() =>true,  
+    },
     fields: [
         {
             name: "role",
+            defaultValue:"user",
+            required:true, 
+            admin: {
+              condition:()=>false,
+            },
             type: "select",
             options: [
                 { label: "Admin", value: "admin" },
