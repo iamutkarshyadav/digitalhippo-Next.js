b94d607337478a0651cb4fa7c78dbc25eaee3f46
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-04-23 04:43:50 +0530

    Fix form submission in sign-up page and update dependencies

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -14,11 +14,10 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/lib/validators/account-credentials-validator";
-
+import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
 import { ZodError } from "zod";
 import { useRouter } from "next/navigation";
-import { trpc } from "@/trpc/client";
 
 const Page = () => {
   const {
@@ -31,15 +30,30 @@ const Page = () => {
 
   const router = useRouter();
 
-  const { mutate, isLoading } = trpc
-    .auth
-    .createPayloadUser
-    .useMutation({
-  })
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) => {
+      if (err.data?.code === "CONFLICT") {
+        toast.error("This email is already in use. Sign in instead?");
+
+        return;
+      }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+
+        return;
+      }
+
+      toast.error("Something went wrong. Please try again.");
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}.`);
+      router.push("/verify-email?to=" + sentToEmail);
+    },
+  });
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     mutate({ email, password });
-    // sends this data to server
   };
 
   return (
