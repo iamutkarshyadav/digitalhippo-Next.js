fce31d84545c42aee2a43a0b9edae02eddebff87
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-04-23 04:05:21 +0530

    Refactor Users collection configuration in Users.ts

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,25 +1,31 @@
 import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
-    slug: "users", 
-    auth: true, 
-    access: {
-      read: () => true,
-      create:() =>true,  
+  slug: "users",
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>helllo pls verify <p>`;
+      },
     },
-    fields: [
-        {
-            name: "role",
-            defaultValue:"user",
-            required:true, 
-            admin: {
-              condition:()=>false,
-            },
-            type: "select",
-            options: [
-                { label: "Admin", value: "admin" },
-                { label: "User", value: "user" },
-            ],
-        },
-    ],
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      defaultValue: "user",
+      required: true,
+      admin: {
+        condition: () => false,
+      },
+      type: "select",
+      options: [
+        { label: "Admin", value: "admin" },
+        { label: "User", value: "user" },
+      ],
+    },
+  ],
 };
